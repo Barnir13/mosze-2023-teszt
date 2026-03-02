fa41d8b91f9110951259c0baaba108030a45588a
Barnir <xxbarnirxx@gmail.com>
2026-03-02 18:46:48 +0100

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,31 +4,32 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // NELEMENTS helyett N_ELEMENTS
+    int *b = new int[N_ELEMENTS];
 
-    std::cout << '1-100 ertekek duplazasa' // karakterliterál helyett string kell (" ") és hiányzik pontosvessző
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
 
-    for (int i = 0;) //for ciklus szintaxisa rossz, hiányzik a feltétel és inkrementálás
+    // Értékek feltöltése duplázva
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
 
-    for (int i = 0; i; i++) //ciklus feltétele rossz, i; nem értelmezhető
+    // Értékek kiíratása
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" //hiányzik az érték kiírása, a pontosvessző és endl
-    }    
-
-    std::cout << "Atlag szamitasa: " << std::endl;
-
-    int atlag; //nincs inicializálva, így random értékkel indul
-    for (int i = 0; i < N_ELEMENTS, i++) //vessző helyett pontosvessző kell a ciklusban
-    {
-        atlag += b[i] //hiányzik a pontosvessző
+        std::cout << "Ertek[" << i << "]: " << b[i] << std::endl;
     }
 
+    // Átlag számítása
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        atlag += b[i];
+    }
     atlag /= N_ELEMENTS;
 
     std::cout << "Atlag: " << atlag << std::endl;
 
-    return 0; // nem szabad elfelejteni delete[] b a memória felszabadításához
+    delete[] b; // memória felszabadítása
+    return 0;
 }
