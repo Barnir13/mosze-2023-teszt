601c1b9320e9a300add7f7884e39527494c9bdc7
Barnir <xxbarnirxx@gmail.com>
2026-03-02 18:38:13 +0100

    Kommentelt hibák

--- main.cpp
+++ main.cpp
@@ -4,23 +4,31 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // NELEMENTS helyett N_ELEMENTS
+
+    std::cout << '1-100 ertekek duplazasa' // karakterliterál helyett string kell (" ") és hiányzik pontosvessző
+
+    for (int i = 0;) //for ciklus szintaxisa rossz, hiányzik a feltétel és inkrementálás
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+
+    for (int i = 0; i; i++) //ciklus feltétele rossz, i; nem értelmezhető
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzik az érték kiírása, a pontosvessző és endl
     }    
+
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+
+    int atlag; //nincs inicializálva, így random értékkel indul
+    for (int i = 0; i < N_ELEMENTS, i++) //vessző helyett pontosvessző kell a ciklusban
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzik a pontosvessző
     }
+
     atlag /= N_ELEMENTS;
+
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+
+    return 0; // nem szabad elfelejteni delete[] b a memória felszabadításához
 }
